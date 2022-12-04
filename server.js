a980b087e04260d0d79e538260fc5c1a1dcfe215
nathan hishon <nathan.hishon@gmail.com>
2022-12-04 12:48:33 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+
+const app = express();
+
+
+var corsOptions = {
+  origin: process.env.CLIENT_URL
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  const yo = tu
+  res.json({ message: "yo" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
