9db60aa51ef7d76ff7593e3adc9e70b8389b60dc
nathan hishon <nathan.hishon@gmail.com>
2022-12-04 14:16:52 -0500

    update4

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  host: process.env.DB_HOST, // This was changed
-  port: 3306,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
